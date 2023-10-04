395cfb5ccfaeb0c4e23a1349c675c054afdc9652
nikitafedotov <nikitka.kozarezov@mail.ru>
2023-10-04 20:50:56 +0200

    feat(base-loader): add tests

---
+++ src/components/Dropdown/BaseDropdown/BaseDropdown.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div>
+    this is gonna be a dropdown
+  </div>
+</template>
+<style scoped lang="scss" src="./styles.scss" />
--- src/components/Loader/BaseLoader/BaseLoader.test.ts
+++ src/components/Loader/BaseLoader/BaseLoader.test.ts
@@ -1,4 +1,32 @@
+import {render, screen, waitFor } from "@testing-library/vue";
+import "@testing-library/jest-dom"
+import BaseLoader from "./BaseLoader.vue";
+import BaseLoaderProps from "@/types/props/Loader/BaseLoaderProps";
+import useColor from "../../../composables/useColor";
+const { getColor } = useColor();
+let defaultProps: BaseLoaderProps = {
+    color: 'primary',
+    borderWidth: '1px',
+    size: '25px',
+    label: 'Loading',
+};
+function create(props = defaultProps,) {
+    const { container } = render(BaseLoader, { props });
+    const loader = container.firstChild as HTMLElement;
+    return { loader, container }
+}
 describe('Loader: Base Loader', () => {
-    it.todo('Renders label');
-    it.todo('Changes color');
+    it('Renders label', () => {
+        const { loader } = create();
+        expect(loader).toHaveTextContent(defaultProps.label);
+    });
+    it('Changes color', () => {
+        const props: BaseLoaderProps = { color: 'primary' }
+        const { loader } = create(props);
+        expect(loader).toHaveStyle({ '--accent-color':  getColor(props.color).join(', ')})
+        props.color = "alert"
+        waitFor(() => {
+            expect(loader).toHaveStyle({ '--accent-color':  getColor(props.color).join(', ')})
+        })
+    });
 });
