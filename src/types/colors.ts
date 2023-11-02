384ac08c6488108cb1e1a25ec941c777e3fca16e
Nikita Fedotov <nikitka.kozarezov@mail.ru>
2023-11-03 00:33:26 +0300

    feat(colors): white color

--- src/types/colors.ts
+++ src/types/colors.ts
@@ -5,6 +5,7 @@ interface IColors {
     secondary: Color,
     success: Color,
     warning: Color,
+    white: Color,
     none: Color,
 };
 const colors: IColors = {
@@ -13,6 +14,7 @@ const colors: IColors = {
     secondary: [77, 69, 241],
     success:   [34, 197, 94],
     warning:   [249, 115, 22],
+    white:     [255, 255, 255],
     none:      [120, 120, 120],
 };
 export { type IColors, type Color };
