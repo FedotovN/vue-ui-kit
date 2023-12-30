5b9058fb376e97e34f5b11c49504ddb78ef2433f
Nikita Fedotov <nikitka.kozarezov@mail.ru>
2023-12-30 20:59:56 +0300

    fix(storybook): add config

---
+++ .storybook/main.ts
@@ -0,0 +1,29 @@
+import type { StorybookConfig } from "@storybook/vue3-vite";
+import plugin from "@vitejs/plugin-vue-jsx";
+import { PluginOption } from "vite";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  async viteFinal(config) {
+    config.plugins = [
+      ...[config.plugins].flat().filter((p) => !Array.isArray(p)),
+      plugin() as any,
+      {
+        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+      },
+    ];
+    return config;
+  },
+};
+export default config;
