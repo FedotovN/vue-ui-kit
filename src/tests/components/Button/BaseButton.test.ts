b7bc0fb2142e63879149ae3e666afc31fb0404b9
Nikita Fedotov <nikitka.kozarezov@mail.ru>
2023-12-31 21:50:21 +0300

    HAPPY NEW YEAR 2024

---
+++ src/tests/components/Button/BaseButton.test.ts
@@ -0,0 +1,4 @@
+import { describe, it } from "vitest";
+describe("Default dehavior", () => {
+  it("Renders", () => {});
+});
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
 import path from "path";
 // https://vitejs.dev/config/
+/// <reference types="vitest" />
 export default defineConfig({
   plugins: [
     vue(),
